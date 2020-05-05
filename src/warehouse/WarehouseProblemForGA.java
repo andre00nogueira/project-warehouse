72d20ec888aaec17b0b37b6d07a8f3e5df3da2cc
Alexandre Santos <santosalexandre389@gmail.com>
2020-05-05 21:52:32 +0100

    first version of computeFitness and more

--- src/warehouse/WarehouseProblemForGA.java
+++ src/warehouse/WarehouseProblemForGA.java
@@ -9,20 +9,48 @@ public class WarehouseProblemForGA implements Problem<WarehouseIndividual> {
 
     //TODO this class might require the definition of additional methods and/or attributes
     private ArrayList<Request> requests;
-    private int numeroProdutos;
+    private LinkedList<Cell> shelves;
+    private LinkedList<Pair> pairs;
+    private Cell door;
+    private int numProducts;
+
 
     public WarehouseProblemForGA(WarehouseAgentSearch agentSearch) {
-        //TODO
-        numeroProdutos=agentSearch.getNumProducts();
-        requests= new ArrayList<>();
-        System.arraycopy(agentSearch.getRequests(), 0, requests, 0, agentSearch.getRequests().size());
-        requests=agentSearch.getRequests();
-        throw new UnsupportedOperationException("Not implemented yet.");
+        requests = new ArrayList<>(agentSearch.getRequests());
+        shelves = new LinkedList<>(agentSearch.getShelves());
+        pairs = new LinkedList<>(agentSearch.getPairs());
+        numProducts = agentSearch.getNumProducts();
+        door = agentSearch.getExit();
     }
 
     @Override
     public WarehouseIndividual getNewIndividual() {
-        return new WarehouseIndividual(this,requests.size());
+        return new WarehouseIndividual(this, shelves.size());
+    }
+
+    public ArrayList<Request> getRequests() {
+        return requests;
+    }
+
+    public LinkedList<Cell> getShelves() {
+        return shelves;
+    }
+
+    public LinkedList<Pair> getPairs() {
+        return pairs;
+    }
+
+    public Cell getDoor() {
+        return door;
+    }
+
+    public Pair getPairs(Cell first, Cell second) {
+        for (Pair pair : pairs) {
+            if (pair.getCell1() == first && pair.getCell2() == second) {
+                return pair;
+            }
+        }
+        return null;
     }
 
     @Override
